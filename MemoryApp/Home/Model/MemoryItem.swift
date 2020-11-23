258b35577d19c80bee4dcb6ad7a576cd59f588b3
nikolas <nikoagge@gmail.com>
2020-11-23 21:18:25 +0200

    HomeViewController of the app set

---
+++ MemoryApp/Home/Model/MemoryItem.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryItem.swift
+//  MemoryApp
+//
+//  Created by Nikolas Aggelidis on 23/11/20.
+//  Copyright © 2020 NAPPS. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryItem: Codable {
+    var title: String
+    var text: String
+}
